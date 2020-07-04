8f9b45379609bb89dbcadbf251719d575292d83b
AANtoso <adjoaan@gmail.com>
2020-07-04 11:01:08 -0400

    Added relationships.

--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,2 +1,4 @@
 class Diagnosis < ApplicationRecord
+    has_many :medications
+    validates :name, presence: true
 end
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,2 +1,5 @@
 class Medication < ApplicationRecord
+    belongs_to :diagnosis
+    validates :name, presence: true
+    validates :called_in, inclusions: { in: [ true, false ]}
 end
