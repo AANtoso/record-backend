ba5ddb8871dab56722cd8c4a2d5b6cb3df1375d6
AANtoso <adjoaan@gmail.com>
2020-07-04 11:16:36 -0400

    Created diagnoses controller.

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,2 +1,11 @@
-class DiagnosesController < ApplicationController
+class Api::V1::DiagnosesController < ApplicationController
+    def index
+        @diagnoses = Diagnosis.all 
+        render json: @diagnosis 
+    end
+
+    def show
+        @diagnosis = Diagnosis.find(params[:id])
+        render json: @diagnosis, include: [:medications]
+    end
 end
