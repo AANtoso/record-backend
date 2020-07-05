d409ac9db31d4dbc49f396dc8ddf330899557ada
AANtoso <adjoaan@gmail.com>
2020-07-05 10:11:35 -0400

    Created controller actions.

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,36 @@
-class MedicationsController < ApplicationController
+class Api::V1::MedicationsController < ApplicationController
+
+    before_action: set_diagnosis
+
+    def index
+        @medications = @diagnosis.medications 
+        render json: @medications
+    end
+
+    def create
+        @medication = @diagnosis.create(medication_params)
+        render json: @medications
+    end
+
+    def show
+        @medication = diagnosis.medications.find(params[:id])
+        render json: @medication
+    end
+
+    def destroy
+        @medication = @diagnosis.medications.find(params[:id])
+        @medication.destroy
+        render json: @diagnosis
+    end
+
+    private
+
+    def set_diagnosis
+        @diagnosis = Diagnosis.find(params[:diagnosis_id])
+    end
+
+    def medication_params
+        params.require(:mmedication).permit(:name, :dose, :frequency, :diagnosis_id, :called_in)
+    end
+
 end
